9001e185f913c54952aa2c4a9f95c8e68eedac07
tmilewski <tmilewski@gmail.com>
2019-06-07 21:17:50 -0400

    Additional specs for Railtie integration

--- spec/omniauth-rails/railtie_spec.rb
+++ spec/omniauth-rails/railtie_spec.rb
@@ -2,17 +2,48 @@
 require 'spec_helper'
 
 describe OmniAuth::Rails::Railtie do
-  before do
-    OmniAuth::Rails::Railtie.initializers.each(&:run)
+  describe 'Railtie' do
+    before do
+      OmniAuth::Rails::Railtie.initializers.each(&:run)
+    end
+
+    it 'should only allow POST requests' do
+      expect(OmniAuth.config.allowed_request_methods).to eq([:post])
+    end
+
+    it '`before_request_phase` should call `OmniAuth::Rails::RequestForgeryProtection`' do
+      env = {}
+      expect(OmniAuth::Rails::RequestForgeryProtection).to receive(:call).with(env)
+      OmniAuth.config.before_request_phase.call(env)
+    end
   end
 
-  it 'should only allow POST requests' do
-    expect(OmniAuth.config.allowed_request_methods).to eq([:post])
-  end
+  describe 'Railtie Integration' do
+    let(:app) { Rails.application }
 
-  it '`before_request_phase` should call `OmniAuth::Rails::RequestForgeryProtection`' do
-    env = {}
-    expect(OmniAuth::Rails::RequestForgeryProtection).to receive(:call).with(env)
-    OmniAuth.config.before_request_phase.call(env)
+    let(:authenticity_token) do
+      get "/token"
+      last_response.body
+    end
+
+    it 'should disallow access to :get with 404' do
+      get '/auth/developer'
+      expect(last_response.status).to eq(404)
+    end
+
+    it 'should fail as 422 without a token' do
+      post '/auth/developer'
+      expect(last_response.status).to eq(422)
+    end
+
+    it 'should fail with a bad token' do
+      post '/auth/developer', authenticity_token: 'foo'
+      expect(last_response.status).to eq(422)
+    end
+
+    it 'should succeed with a vaild token' do
+      post '/auth/developer', authenticity_token: authenticity_token
+      expect(last_response.status).to eq(200)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,44 @@ require 'omniauth'
 
 require 'omniauth-rails/railtie'
 require 'omniauth-rails/request_forgery_protection'
+
+class TestApp < Rails::Application
+  config.root = __dir__
+  config.session_store :cookie_store, key: 'cookie_store_key'
+  secrets.secret_key_base = 'secret_key_base'
+  config.eager_load = false
+  config.hosts = []
+
+  # This allow us to send all logs to STDOUT if we run test wth `VERBOSE=1`
+  config.logger = if ENV['VERBOSE']
+                    Logger.new($stdout)
+                  else
+                    Logger.new('/dev/null')
+                  end
+  Rails.logger = config.logger
+  OmniAuth.config.logger = Rails.logger
+
+  # Setup a simple OmniAuth configuration with only developer provider
+  config.middleware.use OmniAuth::Builder do
+    provider :developer
+  end
+
+  # We need to call initialize! to run all railties
+  initialize!
+
+  # Define our custom routes. This needs to be called after initialize!
+  routes.draw do
+    get "token" => "application#token"
+  end
+end
+
+# A small test controller which we use to retrive the valid authenticity token
+class ApplicationController < ActionController::Base
+  def token
+    render plain: form_authenticity_token
+  end
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end
